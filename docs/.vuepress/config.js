da74c55a66e984a221e73c1486dac11d1dc6b5f8
范月椿 <fyc_115@163.com>
2020-07-11 08:15:07 +0800

    面试题

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,11 +3,16 @@ module.exports = {
     head: [ // 注入到当前页面的 HTML <head> 中的标签
       ['link', { rel: 'icon', href: '/1.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
     ],
-    base:'/fyc-Away/',
+    base:'/',
     themeConfig: {
       logo: '/1.jpg',  // 左上角logo
       nav:[ // 导航栏配置
         {text: '首页', link: '/' },
+        {text: '基础篇', link: '/basics' },
+        {text: '进阶篇', link: '/advanced' },
+        {text: '高级篇', link: '/top' },
+        {text: '精简篇', link: '/jian' },
+        {text: '进阶篇', link: '/advanced' },
         {text: 'vue', link: '/vue' },
         {text: 'react', link: '/react' },
         {text: '其它', link: '/other' },
